34f6c7d2739820d6864a43ec144a046333195a62
Dindin4987 <144334821+Dindin4987@users.noreply.github.com>
2024-09-22 16:12:06 +0800

    update

--- src/redux/authSlice.js
+++ src/redux/authSlice.js
@@ -1,5 +1,6 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
+import { jwtDecode } from "jwt-decode";
 
 const initialState = {
     user: null,
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,35 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import expensesReducer from '../redux/expensesSlice';
 import incomeReducer from '../redux/incomeSlice';
 import authReducer from '../redux/authSlice'
+import { combineReducers } from '@reduxjs/toolkit';
+
+// Configure persist for the auth state
+const persistConfig = {
+    key: 'root',
+    storage,
+};
+
+// Combine reducers
+const rootReducer = combineReducers({
+    expenses: expensesReducer,
+    income: incomeReducer,
+    auth: authReducer,
+});
+
+// Create a persisted reducer
+const persistedReducer = persistReducer(persistConfig, rootReducer)
 
 export const store = configureStore({
-    reducer: {
-        expenses: expensesReducer,
-        income: incomeReducer,
-        auth: authReducer,
-    },
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: false,
+        }),
 });
+
+export const persistor = persistStore(store);
+
+export default store;
